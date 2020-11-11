d1c649f8a24093e56c8dfade39b3c99d439b4b7d
IlyaBabich1994 <57317239+IlyaBabich1994@users.noreply.github.com>
2020-11-12 02:00:48 +0300

    Add files via upload

---
+++ spring-helloworld/src/main/java/App.java
@@ -0,0 +1,17 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld newBean = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat newCatBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean.equals(newBean));
+        System.out.println(catBean.equals(newCatBean));
+    }
+}
